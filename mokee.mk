1db76bb07571899ae05884c21e1d55ca524d8c4d
martincz <martincz@mokeedev.com>
2016-10-06 03:00:26 +0800

    arm64: mokee bringup
    Change-Id: Ia62160929dfe1da322296b12f33e81484a11f654

--- mokee.mk
+++ mokee.mk
@@ -1,12 +1,12 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/generic/arm64/mini_arm64.mk)
 
-PRODUCT_NAME := cm_arm64
+PRODUCT_NAME := mk_arm64
 PRODUCT_DEVICE := arm64
-PRODUCT_BRAND := CyanogenMod
+PRODUCT_BRAND := MoKee
 PRODUCT_MODEL := arm64
 
 bacon: droid
